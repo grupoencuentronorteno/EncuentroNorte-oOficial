60db5c4de79f5ae3796b72254e55881f8d89250a
grupoencuentronorteno <grupoencuentronorteno@gmail.com>
2026-03-02 23:09:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+function contacto() {
+  alert("Tel: +52 81 0000 0000\nEmail: encuentro.norteno@email.com");
+}
+
+function scrollToContact() {
+  document.getElementById("contacto").scrollIntoView({behavior: "smooth"});
+}
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 3;
+    this.speedY = Math.random() * 1 + 0.5;
+  }
+
+  update() {
+    this.y += this.speedY;
+    if (this.y > canvas.height) {
+      this.y = 0;
+      this.x = Math.random() * canvas.width;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function init() {
+  for (let i = 0; i < 150; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particlesArray.forEach(p => {
+    p.update();
+    p.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
